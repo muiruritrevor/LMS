8b5c488650ae9de82353b8fc1e5654a62d952968
muiruritrevor <trevormuirurii@gmail.com>
2024-10-02 15:05:05 +0300

    add date_of_membership field on user model

--- config/accounts/models.py
+++ config/accounts/models.py
@@ -47,8 +47,9 @@ class User(AbstractUser):
     # Additional fields
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True, max_length=255)
+    email = models.EmailField(unique=True, max_length=255) 
     username = models.CharField(max_length=255, unique=True)
+    date_of_membership = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
