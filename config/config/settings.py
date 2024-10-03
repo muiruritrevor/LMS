6594329e401b351dfd078d87d8b343d8a1baa28f
muiruritrevor <trevormuirurii@gmail.com>
2024-10-03 13:30:18 +0300

    Set custom user model AUTH_USER_MODEL to accounts.User

--- config/config/settings.py
+++ config/config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'library',
+    'rest_framework',
 
 ]
 
@@ -78,10 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Library',
+        'USER': 'root',
+        'PASSWORD': 'tking',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
