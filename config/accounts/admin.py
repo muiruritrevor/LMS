afb91808d507a393f648b91f57765d4fcb9b0bab
muiruritrevor <trevormuirurii@gmail.com>
2024-10-03 18:35:51 +0300

    add Profile

--- config/accounts/admin.py
+++ config/accounts/admin.py
@@ -3,10 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 from .models import *
 
 class UserAdmin(UserAdmin):
-    list_display = ('email', 'username')
+    list_display = ('email', 'username', 'is_staff')
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
- 
 
 admin.site.register(User, UserAdmin) 
+admin.site.register(Profile)
