f60a206e554d0df9bc9ada4a1de7ecc9558a6e6c
muiruritrevor <trevormuirurii@gmail.com>
2024-10-04 20:51:40 +0300

    add browsable api url

--- config/config/urls.py
+++ config/config/urls.py
@@ -24,6 +24,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('auth/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth/', include("rest_framework.urls", namespace="login")),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/', include('api.urls')),
